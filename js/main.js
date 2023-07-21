ebc4ccbfcb1fdcc5138d3e813ba8e21d85178be7
davronme <davronergashev232@gmail.com>
2023-07-21 12:19:18 +0500

    add url copy click change bgc changa content text and timeot

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const elUrlShortnerForm = document.querySelector('.js-url-shortner__form');
+
+const elUrlShortnerResults = document.querySelector('.url-shortner__results');
+
+const elUrlShortner = document.querySelector('.url-shortner')
+
+
+if (elUrlShortnerForm) {
+    elUrlShortnerForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // console.log('text');
+        elUrlShortnerResults.classList.add('url-shortner__results--open');
+    });
+}
+
+if (elUrlShortner) {
+    elUrlShortner.addEventListener('click', function(event) {
+        if (event.target.matches('.js-short-link-copy')) {
+
+            // CHANGA BUTTON TEXT
+            event.target.textContent = 'Copied!';
+
+            // CHANGA BUTTON BGCOLOR
+            event.target.classList.add('button-smoll-rectangle-copied')
+
+            // RESET BUTTON AND CLASS AFTER 1 S
+            setTimeout(function () {
+                event.target.textContent = ('Copy');
+                event.target.classList.remove('button-smoll-rectangle-copied');
+            }, 1000);
+        };
+    });
+
+}
